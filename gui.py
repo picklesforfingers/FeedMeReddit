fb35dcb8b6f4ca51a126e6008ba7841f7b53ff46
picklesforfingers <tobywsax@gmail.com>
2014-10-16 14:50:56 -0700

    Update gui.py
    Updated Documentation

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 """This is the gui that everything displays on. It uses the tkinter framework
 
-Last Updated 15 October 2014
+Last Updated 16 October 2014
 """
 
 from tkinter import *
@@ -17,12 +17,14 @@ class Display(Frame):
 
     """Opens the address of the reddit post picked"""
     def urlOpen(self, feed, var):
-        print("hi there, everyone!")
         webbrowser.open(feed[var], new=0, autoraise=True)
 
     """Creates button"""
     def createWidgets(self):
+        #Grabs titles and urls
         self.feed = self.getPickle()
+        
+        #Creates buttons
         for x in range(len(self.feed[0])):
             self.var = x
             self.button = Button(self)
@@ -32,20 +34,25 @@ class Display(Frame):
 
     """Gets pickle data"""
     def getPickle(self):
+        #Temporary gui initialization
         feeder.gethistory(20, 'programming')
         manager.picklefix()
+        
+        #Loads Titles
         self.title = open("title.obj", "rb")
         self.thist = pickle.load(self.title)
         self.title.close()
 
+        #Loads URLS
         self.url = open("url.obj", "rb")
         self.uhist = pickle.load(self.url)
         self.url.close()
+    
         self.list = [self.thist, self.uhist]
 
         return self.list
 
-
+#Frame Loop
 app = Tk()
 gui = Display(master=app)
 gui.mainloop()
